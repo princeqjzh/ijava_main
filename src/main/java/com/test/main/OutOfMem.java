05ef43506d6e773fe2040f5a0d5731b4b1f4ba26
princeqjzh <11547299@qq.com>
2023-07-03 16:29:17 +0800

    Update

--- src/main/java/com/test/main/OutOfMem.java
+++ src/main/java/com/test/main/OutOfMem.java
@@ -7,19 +7,10 @@ public class OutOfMem {
     public static void main(String[] args) throws InterruptedException {
         // vm args: -Xms2m -Xmx2m -> Heap Out of Memory
         int count = 0;
-        int gc_count = 0;
         List list = new ArrayList();
         while(count < 50000){
             list.add(new Object());
             System.out.println("Count = " + ++count);
-//            gc_count++;
-//            if(gc_count >= 10000){
-//                System.gc();
-//                gc_count = 0;
-//                System.out.println("Trigger GC!");
-//                Thread.sleep(500);
-//            }
-//            Thread.sleep(500);
         }
     }
 }
